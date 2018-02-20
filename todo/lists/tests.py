5fc23b39e98fdbb46832d67416051129aac1b037
Roman Synovets <hvllowe3n@Romans-MacBook-Air.local>
2018-02-20 16:54:39 +0200

    RED add test fot view HTML response

--- todo/lists/tests.py
+++ todo/lists/tests.py
@@ -1,17 +1,24 @@
 from django.test import TestCase
-from django.urls import resolve
+from django.urls import resolve, reverse
 from lists.views import home_page
 
-# when we go to url '/' 200 code is sent back
 
 class HomePageTest(TestCase):
-
+	# when we go to url '/' 200 code is sent back
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 
 		self.assertEqual(found.func, home_page)
 
-# Create your tests here.
+
+	# Когда user переходит по url ('/') то отобразить HttpResponse
+	def test_when_user_go_to_root_url(self):
+		response = self.client.get(reverse('home_page'))
+
+		get_html = response.content.decode('utf8')
+
+		self.assertIn('Hello World', get_html)
+
 
 
 
